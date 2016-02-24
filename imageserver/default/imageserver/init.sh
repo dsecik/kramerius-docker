fdc65d662cb8094d9c032adfd7014f42b400f531
Daniel Secik <dsecik@gmail.com>
2016-02-24 10:29:58 +0100

    Using new package from klokantech and upgraded openseadragon to 2.1

--- imageserver/default/imageserver/init.sh
+++ imageserver/default/imageserver/init.sh
@@ -1,5 +1,4 @@
  #/bin/bash
 
-/usr/bin/spawn-fcgi -f /usr/lib/cgi-bin/iipsrv.fcgi -p $PORT -F $CHILDREN
-
-exec tail -F /var/log/iipsrv/iipsrv.log
+/usr/bin/spawn-fcgi -f /usr/share/cgi-bin/iiifserver.fcgi -p $PORT -F $CHILDREN
+exec tail -F /tmp/iipsrv.log
