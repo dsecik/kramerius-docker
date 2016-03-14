fd507c33270e1e777bba57e9b55f93cba9c0a102
Martin Rumanek <martin@rumanek.cz>
2016-03-14 19:52:49 +0100

    tmp666

--- fcrepo/3.8/docker-entrypoint.sh
+++ fcrepo/3.8/docker-entrypoint.sh
@@ -7,7 +7,7 @@ fi
 if [ -n "$RITRIPLES_DB_HOST" ] && [ -n "$RITRIPLES_DB_NAME" ] ; then
 xmlstarlet ed -L -N x="http://www.fedora.info/definitions/1/0/config/" \
   -u "//x:datastore[@id='localPostgresMPTTriplestore']/x:param[@name='jdbcURL']/@value" -v "jdbc:postgresql://$RITRIPLES_DB_HOST/$RITRIPLES_DB_NAME" \
-  $FEDORA_HOME/server/config/fedora-users.xml
+  $FEDORA_HOME/server/config/fedora.fcfg
 fi
 
 exec "$@"
