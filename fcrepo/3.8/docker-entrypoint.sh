a8ab21f30f45ec3dec8579f1dccf71e127541f05
Martin Rumanek <martin@rumanek.cz>
2016-03-14 19:34:29 +0100

    tmp5

--- fcrepo/3.8/docker-entrypoint.sh
+++ fcrepo/3.8/docker-entrypoint.sh
@@ -4,4 +4,10 @@ if [ -n "$FEDORA_PASSWORD" ] ; then
   xmlstarlet ed  --inplace -u "/users/user/@password" -v $FEDORA_PASSWORD $FEDORA_HOME/server/config/fedora-users.xml 
 fi
 
+if [ -n "$RITRIPLES_DB_HOST" ] && [ -n "$RITRIPLES_DB_NAME" ] ; then
+xmlstarlet ed -L -N x="http://www.fedora.info/definitions/1/0/config/" \
+  -u "//x:datastore[@id='localPostgresMPTTriplestore']/x:param[@name='jdbcURL']/@value" -v "jdbc:postgresql://$RITRIPLES_DB_HOST/$RITRIPLES_DB_NAME" \
+  $FEDORA_HOME/server/config/fedora-users.xml
+fi
+
 exec "$@"
