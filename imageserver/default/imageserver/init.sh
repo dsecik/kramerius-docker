2191686f34572407a001a08eb18f90f7afa03fe9
Daniel Sečík <dsecik@gmail.com>
2016-03-15 12:31:23 +0100

    Update init.sh

--- imageserver/default/imageserver/init.sh
+++ imageserver/default/imageserver/init.sh
@@ -1,4 +1,4 @@
  #/bin/bash
 
-/usr/bin/spawn-fcgi -f /usr/lib/cgi-bin/iipsrv.fcgi -p $PORT -F $CHILDREN
+/usr/bin/spawn-fcgi -f /usr/lib/cgi-bin/iipsrv.fcgi -p 9000 -F 1
 exec tail -F /tmp/iipsrv.log
