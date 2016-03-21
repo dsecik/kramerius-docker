28d607a8b8c557452b2da97d9080fd95c1ecdebe
Martin Rumanek <martin@rumanek.cz>
2016-03-21 09:07:23 +0100

    fedora_db env

--- fcrepo/3.8/docker-entrypoint.sh
+++ fcrepo/3.8/docker-entrypoint.sh
@@ -10,4 +10,10 @@ xmlstarlet ed -L -N x="http://www.fedora.info/definitions/1/0/config/" \
   $FEDORA_HOME/server/config/fedora.fcfg
 fi
 
+if [ -n "$FEDORA_DB_HOST" ] && [ -n "$FEDORA_DB_NAME" ] ; then
+xmlstarlet ed -L -N x="http://www.fedora.info/definitions/1/0/config/" \
+  -u "//x:datastore[@id='localPostgreSQLPool']/x:param[@name='jdbcURL']/@value" -v "jdbc:postgresql://$FEDORA_DB_HOST:5432/$FEDORA_DB_NAME" \
+  $FEDORA_HOME/server/config/fedora.fcfg
+fi
+
 exec "$@"
